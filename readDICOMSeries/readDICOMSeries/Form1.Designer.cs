ff9ae397da29f02f0a40b6917ed2275cf6a2f36d
Houssk <karrach.houssam@gmail.com>
2015-11-23 09:23:59 +0100

    the first commit

---
+++ readDICOMSeries/readDICOMSeries/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace readDICOMSeries
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.renderWindowControl1 = new Kitware.VTK.RenderWindowControl();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.renderWindowControl2 = new Kitware.VTK.RenderWindowControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // renderWindowControl1
+            // 
+            this.renderWindowControl1.AddTestActors = false;
+            this.renderWindowControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.renderWindowControl1.Location = new System.Drawing.Point(12, 38);
+            this.renderWindowControl1.Name = "renderWindowControl1";
+            this.renderWindowControl1.Size = new System.Drawing.Size(315, 276);
+            this.renderWindowControl1.TabIndex = 0;
+            this.renderWindowControl1.TestText = null;
+            this.renderWindowControl1.Load += new System.EventHandler(this.renderWindowControl1_Load);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1250, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(103, 22);
+            this.toolStripButton1.Text = "Ouvrir une image";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // renderWindowControl2
+            // 
+            this.renderWindowControl2.AddTestActors = false;
+            this.renderWindowControl2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.renderWindowControl2.Location = new System.Drawing.Point(879, 38);
+            this.renderWindowControl2.Name = "renderWindowControl2";
+            this.renderWindowControl2.Size = new System.Drawing.Size(338, 276);
+            this.renderWindowControl2.TabIndex = 2;
+            this.renderWindowControl2.TestText = null;
+            this.renderWindowControl2.Load += new System.EventHandler(this.renderWindowControl2_Load);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Filtre";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1250, 326);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.renderWindowControl2);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.renderWindowControl1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Form1";
+            this.Text = "ITK-VTK";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Kitware.VTK.RenderWindowControl renderWindowControl1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private Kitware.VTK.RenderWindowControl renderWindowControl2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ readDICOMSeries/readDICOMSeries/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Kitware.VTK;
+using itk.simple;
+
+
+namespace readDICOMSeries
+{
+    
+    public partial class Form1 : Form
+    {
+        public vtkProp3D imgProp = null;
+        vtkImageViewer2 _ImageViewer;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+          if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {        
+                string fileName = openFileDialog1.FileName;
+                vtkDICOMImageReader reader = vtkDICOMImageReader.New();
+                reader.SetFileName(fileName);
+                reader.Update();
+                _ImageViewer = vtkImageViewer2.New();
+                _ImageViewer.SetInputConnection(reader.GetOutputPort());
+                vtkRenderWindow renderWindow = renderWindowControl1.RenderWindow;
+                _ImageViewer.SetRenderWindow(renderWindow);
+                _ImageViewer.Render();
+            }
+        }
+       
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (imgProp != null)
+            {
+                imgProp.Dispose();
+            }
+            if (this.renderWindowControl1 != null)
+            {
+                this.renderWindowControl1.Dispose();
+            }
+            System.GC.Collect();
+        }
+
+        private void renderWindowControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void renderWindowControl2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            itk.simple.Image image = SimpleITK.ReadImage(this.openFileDialog1.FileName);
+            image = SimpleITK.DiscreteGaussian(image);
+            string fileName = "C:\\Users\\Utilisateur\\Desktop\\matlab\\test101.dcm";
+            SimpleITK.WriteImage(image, fileName);
+            vtkDICOMImageReader reader = vtkDICOMImageReader.New();
+            reader.SetFileName(fileName);
+            reader.Update();
+            _ImageViewer = vtkImageViewer2.New();
+            _ImageViewer.SetInputConnection(reader.GetOutputPort());
+            vtkRenderWindow renderWindow = renderWindowControl2.RenderWindow;
+            _ImageViewer.SetRenderWindow(renderWindow);
+            _ImageViewer.Render();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
